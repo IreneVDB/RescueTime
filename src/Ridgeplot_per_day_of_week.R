9b06b28bd9c0040af37a0771eca68552834317d1
IreneVDB <irenevandenbroek@gmail.com>
2021-09-05 15:29:23 +0200

    add img + caption

--- src/Ridgeplot_per_day_of_week.R
+++ src/Ridgeplot_per_day_of_week.R
@@ -3,16 +3,17 @@ library(ggtext)
 library(pins)
 library(tidyverse)
 library(lubridate)
+#library(patchwork)
 
 # make ggridge plot from time on PC vs time on Mobile:
 
 RescueTime <- pin_get("RescueTime")
 
 pc_Times <- bind_rows("laptop" = RescueTime[["pc"]], 
-                      "mobile device" = RescueTime[["mobile"]], .id = "device") 
+                      "mobile devices" = RescueTime[["mobile"]], .id = "device") 
 
 ggridge.weekday.identity <- function(df, group_by, count_missing_days = NA, tracker, 
-                                     time_interval=5, hues, starttime=0){
+                                     time_interval=5, hues, starttime=0, font="Comfortaa"){
   
   if(any(is.na(count_missing_days))){
     count_missing_days <- sapply(unique(df[[group_by]]), is.character, 
@@ -76,35 +77,43 @@ ggridge.weekday.identity <- function(df, group_by, count_missing_days = NA, trac
   if(length(unique(Timebin$ndays)) == 1){
     subtitle_add <- unique(Timebin$ndays)
   } else{
-    subtitle_add <- paste("<span style = 'color: ", colors[1], ";'>", unique(Timebin$ndays)[1], 
+    subtitle_add <- paste0("<span style = 'color: ", colors[1], ";'>", unique(Timebin$ndays)[1], 
                           "</span>/<span style = 'color: ", colors[2], ";'>", unique(Timebin$ndays)[2], "</span>")
   }
   
-  title <- paste0("Time on <span style = 'color: ", colors[1], ";'>", levels(Summary$group)[1], 
+  title <- paste0("How much time do I spend on my <span style = 'color: ", colors[1], ";'>", levels(Summary$group)[1], 
                   "</span> and <span style = 'color: ",
-                  colors[2], ";'>", levels(Summary$group)[2], "</span> per day of week")
-  subtitle <- paste0("Measured with ", tracker, " (", subtitle_add, " days)")
+                  colors[2], ";'>", levels(Summary$group)[2], "</span>?")
+  
+  subtitle <- paste0("Measured with ", "<img src='img/", tracker, ".png' width='20'/> ", 
+                     tracker, " (", subtitle_add, " days)")
+  
+  caption <- "Graphic: Irene van den Broek | @JeBentWatJeMeet"
   
   # make the plot:
   breaks <- seq(0, (24 - starttime) * (60 / time_interval) - 1, by = 60 / time_interval)
+  r_mar <- 70
+  l_mar <- 10
   
   ggplot(data = df_time, aes(x = row, y = Weekday)) + 
     geom_density_ridges(aes(fill = group, color = group,  height = Time),
                         stat="identity", alpha = 0.8, size = 0.2, scale = 0.9, show.legend = FALSE) +
     geom_text(data = Totals, aes(x= (24-starttime) * (12 + 1/4), y = c(1:7 + 1, 1:7 + 0.7),
                                  label = paste(Total_chr,  "/day"),
-                                 hjust=0, vjust=1, family = "Comfortaa", color = group),
+                                 hjust=0, vjust=1, family = font, color = group),
               show.legend = FALSE, size = 4.2) +
     coord_cartesian(clip = "off") +
-    labs(x = "Time of Day", y = NULL, title = title, subtitle = subtitle) +
-    theme_minimal(base_family = "Comfortaa") +
-    theme(plot.margin = unit(c(5, 70, 5, 15), "pt"),
+    labs(x = "Time of Day", y = NULL, title = title, subtitle = subtitle, caption=caption) +
+    theme_minimal(base_family = font) +
+    theme(plot.margin = unit(c(5, r_mar + 5, 5, l_mar + 5), "pt"),
           plot.background = element_rect(fill='transparent', color=NA),
           panel.grid = element_blank(),
-          plot.subtitle = element_markdown(size = 14, hjust = 0, color = "grey30", margin = margin(b = 30)),
           plot.title.position = "plot",
-          plot.title = element_markdown(face = "bold", size = 20, hjust = 0, color = "grey20"),
-          axis.title.x = element_text(face = "bold", margin = margin(t = 6), color = "grey30", size = 12),
+          plot.caption.position = "plot",
+          plot.title = element_markdown(face = "bold", size = 24, hjust = 0, color = "grey20", margin=margin(l=-l_mar)),
+          plot.subtitle = element_markdown(size = 14, hjust = 0, color = "grey30", margin = margin(b = 20, l=-l_mar)),
+          plot.caption = element_markdown(size = 9, hjust=1, color = "grey30", margin=margin(r=-r_mar)),
+          axis.title.x = element_text(face = "bold", margin = margin(t = 6), color = "grey30", size = 14),
           axis.text.x = element_text(size = 10, color = "grey40", margin = margin(t = 5)),
           axis.text.y = element_text(size = 12, color = "grey40", margin = margin(r = 5), vjust = 0),
           axis.ticks.x = element_line(color = "grey60", size = 0.2)) +
@@ -113,9 +122,10 @@ ggridge.weekday.identity <- function(df, group_by, count_missing_days = NA, trac
     scale_fill_manual(values = hsv(h=hues, s = 0.6, v = 0.9)) +
     scale_color_manual(values = hsv(h=hues, s = 0.8, v = 0.7)) 
   }
-  
-png("output/pcTimes.png", width=13, height=8, units="in", res=300)
+
+#png("output/pcTimes.png", width=10, height=6, units="in", res=300)
 ggridge.weekday.identity(df=pc_Times, group_by="device", 
-                         count_missing_days =  list("laptop" = TRUE, "mobile device" = FALSE),
-                         tracker="RescueTime", hues = c(0.53, 0.78), starttime=5)
+                         count_missing_days =  list("laptop" = TRUE, "mobile devices" = FALSE),
+                         tracker="RescueTime", hues = c(0.53, 0.78), starttime=5,
+                         font="Kreon")
 dev.off()
