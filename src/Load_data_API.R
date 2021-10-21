9520136b0399e28b1d596bc5ff52119c9059a17a
IreneVDB <irenevandenbroek@gmail.com>
2021-10-21 12:01:42 +0200

    update RT api code

--- src/Load_data_API.R
+++ src/Load_data_API.R
@@ -4,31 +4,58 @@ library(lubridate)
 library(tidyverse)
 library(pins)
 
-get.RT.api <- function(key, perspective, taxonomy, interval, startdate, enddate, format, device, username, password){
-  url <- paste0("https://www.rescuetime.com/anapi/data?key=", key, "&perspective=", perspective, "&restrict_kind=",
-                taxonomy, "&interval=", interval, "&restrict_begin=", startdate, "&restrict_end=", enddate, 
-                "&format=", format, "&restrict_source_type=", device)
-  get_data <- GET(url, authenticate(username, password, type = "basic"))
-  content <- content(get_data, as = "parsed")
+get.RT.api <- function(key, perspective, taxonomy, interval, startdate, enddate, 
+                       format = "csv", device, username, password){
+  
+  url <- "https://www.rescuetime.com/anapi/data"
+  get <- GET(url, authenticate(username, password, type = "basic"),
+                  query = list(key = key,
+                               perspective = perspective,
+                               restrict_kind = taxonomy,
+                               interval = interval,
+                               restrict_begin = startdate,
+                               restrict_end = enddate,
+                               format = format,
+                               restrict_source_type = device))
+  content <- content(get, as = "parsed")
 }
+
+# limit for 1 min activity data is 1 month - update per month max and repeat update until Sys.Date()
+# with free account limited to 3 month history: need to update at least every ~ 3month!
+# Note: Will not include most recent data from today (to avoid overlap or storage of 1st day of month)
 update.RT <- function(){
   load("data/RescueTime_all.RData")
-  last_date <- as_date(rev(rev(RescueTime_all[["pc"]])[[1]]$Date)[1])
   
-  new_data <- map(c("computers", "mobile"), get.RT.api,
-                  key = key_get("key", keyring= "RescueTime"),
-                  perspective = "interval",
-                  taxonomy = "activity",
-                  interval = "minute",
-                  startdate = last_date,
-                  enddate = Sys.Date(),
-                  format = "csv",
-                  username = key_get("username", keyring= "RescueTime"),
-                  password = key_get("password", keyring= "RescueTime"))
+  last_date <- as_date(rev(rev(RescueTime_all[["pc"]])[[1]]$Date)[1]) # will give duplicates for that day
   
-  RescueTime_all[["pc"]][[paste0(last_date,"to", Sys.Date())]] <- new_data[[1]]
-  RescueTime_all[["mobile"]][[paste0(last_date,"to", Sys.Date())]] <- new_data[[2]]
-  
+  done <- FALSE
+  while(!done){
+    # set end date to last day of month or to yesterday (Sys.Date - days(1))
+    enddate <- ymd(paste(year(last_date), month(last_date), days_in_month(last_date)))
+    if(enddate >= Sys.Date() - days(1)){
+      enddate <- Sys.Date() - days(1)
+    }
+    
+    new_data <- map(c("computers", "mobile"), get.RT.api,
+                    key = key_get("key", keyring= "RescueTime"),
+                    perspective = "interval",
+                    taxonomy = "activity",
+                    interval = "minute",
+                    startdate = last_date,
+                    enddate = enddate,
+                    format = "csv",
+                    username = key_get("username", keyring= "RescueTime"),
+                    password = key_get("password", keyring= "RescueTime"))
+    
+    RescueTime_all[["pc"]][[paste0(last_date,"to", enddate)]] <- new_data[[1]]
+    RescueTime_all[["mobile"]][[paste0(last_date,"to", enddate)]] <- new_data[[2]]
+    
+    if(enddate == Sys.Date() - days(1)){
+      done <- TRUE
+    } else{
+      last_date <- as_date(rev(rev(RescueTime_all[["pc"]])[[1]]$Date)[1]) + days(1)
+    }
+  }
   save(RescueTime_all, file = "data/RescueTime_all.RData")
 }
 update.RT()
