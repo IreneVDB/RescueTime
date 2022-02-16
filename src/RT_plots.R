d5aa84864f668541ad36f3780bfaf7cb04794cba
IreneVDB <irenevandenbroek@gmail.com>
2022-02-16 13:14:23 +0100

    update RT plots

--- src/RT_plots.R
+++ src/RT_plots.R
@@ -5,14 +5,9 @@ library(lubridate)
 library(ggtext)
 library(png)
 library(grid)
-library(reactable) 
-library(htmltools)
-library(sparkline)
-#library(htmlwidgets)
-#library(reactablefmtr)
-#library(tippy)
+library(ggridges)
 
-#source("src/Load_data_API.R")
+source("src/Load_data_API.R")
 
 RescueTime <- pin_get("RescueTime")
 
@@ -263,8 +258,117 @@ make.stacked.pulse <- function(timebin, xcol){
   return(plot)
 }
 
+# Plot 5) Ridgeplot per day of week:
+ggridge.weekday.identity <- function(df, group_by, count_missing_days = NA,
+                                     time_interval = 5, starttime = 0, colors,
+                                     font = "Comfortaa"){
+  
+  if(any(is.na(count_missing_days))){
+    count_missing_days <- sapply(unique(df[[group_by]]), is.character, 
+                                 simplify=FALSE, USE.NAMES = TRUE)
+  }
+  
+  Summary <- df %>%
+    rename(group = as.name(!!group_by)) %>%
+    group_by(group, DateTime) %>%
+    summarise(Time = sum(Time_sec)) %>%
+    mutate(group = fct_reorder(group, Time, .desc=TRUE))
+  
+  # create long df with ALL times including missing days if count_missing_days == TRUE:
+  Timebin <- map_dfr(levels(Summary$group), function(factor){
+    
+    cmd <- count_missing_days[[factor]]
+    
+    if(cmd == TRUE){
+      DateTimes <- seq(ymd_hm(paste(df[which(df[[group_by]] == factor),]$Date[1], "00:00")),
+                       ymd_hm(paste(rev(df[which(df[[group_by]] == factor),]$Date)[1], "23:55")), 
+                       by = paste(time_interval, "min"))
+    } else{
+      DateTimes <- ymd_hm(paste(rep(unique(df[which(df[[group_by]] == factor),]$Date), 
+                                    each = 24 * 60 / time_interval),
+                                paste(seq(0, 24 - time_interval / 60, by = time_interval / 60) %/% 1, 
+                                      round(seq(0, 24 - time_interval / 60, by = time_interval / 60) %% 1 * 60), 
+                                      sep = ":")))
+    }
+    
+    All_Times <- data.frame(DateTime = DateTimes) %>%
+      mutate(group = factor,
+             ndays = length(unique(as_date(DateTimes))))
+  })
+  
+  # Add all times to Summary data:
+  All_data <- left_join(Timebin, Summary, by=c("DateTime", "group")) %>%
+    replace_na(list(Time = 0)) %>%
+    mutate(Date = as_date(DateTime),
+           Timebin = substr(DateTime, 12, 16),
+           Weekday = fct_rev(wday(Date, week_start=1, label=TRUE))) %>%
+    group_by(group, Weekday, Timebin) %>%
+    summarise(Time = mean(Time) / 60 / time_interval, .groups = "drop_last") %>% # minutes per bin
+    mutate(row = row_number() - 1) 
+  
+  # Summarise total Time based on all days:
+  Totals <- All_data %>%
+    group_by(group, Weekday) %>%
+    summarise(TotalTime = sum(Time) * time_interval /60) %>% # total in hours (~AUC)
+    mutate(Total_chr = case_when(TotalTime %/% 1 < 1 ~ paste0(round(TotalTime * 60), " min"),
+                                 TRUE ~ paste0(TotalTime %/% 1, "h", 
+                                               str_pad(round(TotalTime %% 1 * 60), 2, "left", "0"))))
+  # Normalize x-axis Time range:
+  df_time <- All_data %>%
+    filter(as.numeric(substr(Timebin, 1, 2)) >= starttime) %>%
+    mutate(row = row - starttime * (60 / time_interval))
+  
+  # Define title and subtitle: 
+  
+  if(length(unique(Timebin$ndays)) == 1){
+    subtitle_add <- unique(Timebin$ndays)
+  } else{
+    subtitle_add <- paste0("<span style = 'color: ", colors[1], ";'>", unique(Timebin$ndays)[1], 
+                           "</span>/<span style = 'color: ", colors[2], ";'>", unique(Timebin$ndays)[2], "</span>")
+  }
+  
+  title <- paste0("Time spent on <span style = 'color: ", colors[1], ";'>", levels(Summary$group)[1], 
+                  "</span> vs. <span style = 'color: ",
+                  colors[2], ";'>", levels(Summary$group)[2], "</span>")
+  
+  subtitle <- paste0("Measured with RescueTime (", subtitle_add, " days)")
+  
+  # make the plot:
+  breaks <- seq(0, (24 - starttime) * (60 / time_interval) - 1, 
+                by = 60 / time_interval)
+  
+  ggplot(data = df_time, aes(x = row, y = Weekday)) + 
+    geom_density_ridges(aes(fill = group, color = group,  height = Time),
+                        stat="identity", alpha = 0.75, size = 0.2, scale = 0.9, show.legend = FALSE) +
+    geom_text(data = Totals, aes(x= (24 - starttime) * (12 + 1/4), y = c(1:7 + 1, 1:7 + 0.7),
+                                 label = paste(Total_chr,  "/day"),
+                                 hjust = 0, vjust = 1, family = font, color = group),
+              show.legend = FALSE, size = 3.5) +
+    coord_cartesian(clip = "off") +
+    labs(x = "Time of Day", y = NULL, title = title, subtitle = subtitle) +
+    theme_minimal(base_family = font) +
+    theme(plot.margin = unit(c(2, 22, 2, 2), "mm"),
+          plot.background = element_rect(fill='transparent', color=NA),
+          panel.grid = element_blank(),
+          plot.title.position = "plot",
+          plot.title = element_markdown(face = "bold", size = 15, hjust = 0, color = "grey10"),
+          plot.subtitle = element_markdown(size = 11, hjust = 0, color = "grey20", margin = margin(b = 20)),
+          axis.title.x = element_text(face = "bold", margin = margin(t = 6), color = "grey40", size = 10),
+          axis.text.x = element_text(size = 8, color = "grey50", margin = margin(t = 5)),
+          axis.text.y = element_text(size = 10, color = "grey30", margin = margin(r = 5), vjust = 0),
+          axis.ticks.x = element_line(color = "grey60", size = 0.2)) +
+    scale_x_continuous(expand = c(0, 0), breaks = breaks, 
+                       labels = df_time$Timebin[breaks + 1]) +
+    scale_y_discrete(expand = c(0,0)) +
+    scale_fill_manual(values = hsv(h = rgb2hsv(col2rgb(colors))[1,], 
+                                   s = rgb2hsv(col2rgb(colors))[2,],
+                                   v = rgb2hsv(col2rgb(colors))[3,])) + #hsv(h=hues, s = 0.6, v = 0.9)) +
+    scale_color_manual(values = hsv(h = rgb2hsv(col2rgb(colors))[1,], 
+                                    s = rgb2hsv(col2rgb(colors))[2,],
+                                    v = rgb2hsv(col2rgb(colors))[3,] - 0.1)) 
+}
 
-# add ggplots 1 to 4 to list for easier input in Rmd: ----
+# add ggplots 1 to 5 into list for easier input in Rmd: ----
 RT_plots <- pin_get("RT_plots")
 RT_plots[["Prod_pulse"]] <- make.alltime.pulse(Productivity[["all_time"]])
 RT_plots[["Prod_beeswarm"]] <- make.beeswarm(df = Productivity[["day"]], 
@@ -273,72 +377,78 @@ RT_plots[["Prod_beeswarm"]] <- make.beeswarm(df = Productivity[["day"]],
                                              col_vec = Productivity_col[-6])
 RT_plots[["Prod_Hourly"]] <- make.stacked.pulse("hour", xcol = Timebin)
 RT_plots[["Prod_Weekly"]] <- make.stacked.pulse("week", xcol = Weekday)
+RT_plots[["LaptopMobile"]] <- ggridge.weekday.identity(df =  bind_rows("laptop" = RescueTime[["pc"]], 
+                                                                       "mobile devices" = RescueTime[["mobile"]], .id = "device"), 
+                                                       group_by = "device", 
+                                                       count_missing_days =  list(
+                                                         "laptop" = TRUE, "mobile devices" = FALSE),
+                                                       colors = Productivity_col[c("Productive", "Very Distracting")],
+                                                       starttime = 5)
+
 pin(RT_plots, "RT_plots")
 
 
 # Plot 5) Main categories with icons and donut chart: ----
-make.donut.RT <- function(plot_h = NA){
-  
-  Category <- RescueTime[["day"]] %>%
-    group_by(Overview, Productivity) %>%
-    summarise(Time_min = sum(Time_sec) / length(all_dates) / 60) %>%
-    mutate(TotTime = sum(Time_min))
-  
-  nhours <- length(all_dates) * sum(Category$Time_min) / 60
-  
-  make.donut <- function(df, category){
-    
-    subset <- df %>%
-      filter(Overview == category) %>%
-      mutate(fraction = Time_min / sum(Time_min))
-    
-    par(mar = c(2, 0, 0, 0), xaxs = "i", yaxs = "i", family = "Comfortaa")
-    
-    pie(subset$fraction, clockwise = TRUE, 
-        col = Productivity_col[-6][unique(as.character(subset$Productivity))], 
-        border = "white",labels = NA, xlim = c(-0.7, 0.7), ylim = c(-0.7, 0.7))
-    symbols(0,0, circles = 0.35, fg = "white", bg = "white", 
-            add = TRUE, inches = FALSE)
-    text(0, -0.9, labels = category, font = 2, col = "grey20",
-         cex = 1, adj=c(0.5, 1), xpd = TRUE)
-    text(0, -1, paste0("\n(", round(unique(subset$TotTime)), " min/day)"), 
-         col = "grey20", cex = 1, adj=c(0.5, 1), xpd = TRUE)
-  }
-  
-  if(is.na(plot_h)){plot_h <- dev.size("cm")[2]}
-    
-  h_title <- 1.5
-  h_legend <- 1.8
-  
-  layout(matrix(c(rep(1, 4), rep(2, 4), 3:14), nrow = 5, ncol =  4, byrow = TRUE),
-         heights = lcm(c(h_title, h_legend)))
-  
-  # first plot is a title + subtitle
-  par(mar = c(0, 0, 0, 0), family = "Comfortaa")
-  plot.new()
-  text(0, 1, "Productivity pulse of the 12 main Categories", 
-       adj = c(0, 1), col = "grey20", font = 2, cex = 2, xpd = TRUE)
-  text(0, 0, paste0("Measured with RescueTime (", length(all_dates), " days)"), 
-       adj = c(0, 0.5), col = "grey30", cex = 1.5, xpd = TRUE)
-  
-  # second plot is the legend:
-  par(mar = c(0, 0, 0, 0))
-  plot.new()
-  legend(0.5, 0.5, legend = levels(Category$Productivity),
-         fill = Productivity_col[levels(Category$Productivity)], 
-         title.adj = 0, title.col = "grey20",
-         y.intersp = 1.2, text.col = "grey30",
-         border = "transparent", ncol=2,
-         cex = 1.2, xjust = 0.5, yjust = 0.5, bty = "n", xpd = TRUE)
-  
-  # then add the individual donut plots:
-  lapply(levels(Category$Overview), function(Overview){
-    image <- readPNG(file.path("img", "Categories", paste0(
-      gsub(" .*$", "", Overview), ".png")))
-    make.donut(df = Category, category = Overview)
-    rasterImage(image, xleft = -0.5, xright = 0.5, ybottom = -0.5, ytop = 0.5)
-   })
-}
-make.donut.RT()
-
-
+# make.donut.RT <- function(plot_h = NA){
+#   
+#   Category <- RescueTime[["day"]] %>%
+#     group_by(Overview, Productivity) %>%
+#     summarise(Time_min = sum(Time_sec) / length(all_dates) / 60) %>%
+#     mutate(TotTime = sum(Time_min))
+#   
+#   nhours <- length(all_dates) * sum(Category$Time_min) / 60
+#   
+#   make.donut <- function(df, category){
+#     
+#     subset <- df %>%
+#       filter(Overview == category) %>%
+#       mutate(fraction = Time_min / sum(Time_min))
+#     
+#     par(mar = c(2, 0, 0, 0), xaxs = "i", yaxs = "i", family = "Comfortaa")
+#     
+#     pie(subset$fraction, clockwise = TRUE, 
+#         col = Productivity_col[-6][unique(as.character(subset$Productivity))], 
+#         border = "white",labels = NA, xlim = c(-0.7, 0.7), ylim = c(-0.7, 0.7))
+#     symbols(0,0, circles = 0.35, fg = "white", bg = "white", 
+#             add = TRUE, inches = FALSE)
+#     text(0, -0.9, labels = category, font = 2, col = "grey20",
+#          cex = 1, adj=c(0.5, 1), xpd = TRUE)
+#     text(0, -1, paste0("\n(", round(unique(subset$TotTime)), " min/day)"), 
+#          col = "grey20", cex = 1, adj=c(0.5, 1), xpd = TRUE)
+#   }
+#   
+#   if(is.na(plot_h)){plot_h <- dev.size("cm")[2]}
+#     
+#   h_title <- 1.5
+#   h_legend <- 1.8
+#   
+#   layout(matrix(c(rep(1, 4), rep(2, 4), 3:14), nrow = 5, ncol =  4, byrow = TRUE),
+#          heights = lcm(c(h_title, h_legend)))
+#   
+#   # first plot is a title + subtitle
+#   par(mar = c(0, 0, 0, 0), family = "Comfortaa")
+#   plot.new()
+#   text(0, 1, "Productivity pulse of the 12 main Categories", 
+#        adj = c(0, 1), col = "grey20", font = 2, cex = 2, xpd = TRUE)
+#   text(0, 0, paste0("Measured with RescueTime (", length(all_dates), " days)"), 
+#        adj = c(0, 0.5), col = "grey30", cex = 1.5, xpd = TRUE)
+#   
+#   # second plot is the legend:
+#   par(mar = c(0, 0, 0, 0))
+#   plot.new()
+#   legend(0.5, 0.5, legend = levels(Category$Productivity),
+#          fill = Productivity_col[levels(Category$Productivity)], 
+#          title.adj = 0, title.col = "grey20",
+#          y.intersp = 1.2, text.col = "grey30",
+#          border = "transparent", ncol=2,
+#          cex = 1.2, xjust = 0.5, yjust = 0.5, bty = "n", xpd = TRUE)
+#   
+#   # then add the individual donut plots:
+#   lapply(levels(Category$Overview), function(Overview){
+#     image <- readPNG(file.path("img", "Categories", paste0(
+#       gsub(" .*$", "", Overview), ".png")))
+#     make.donut(df = Category, category = Overview)
+#     rasterImage(image, xleft = -0.5, xright = 0.5, ybottom = -0.5, ytop = 0.5)
+#    })
+# }
+# make.donut.RT()
